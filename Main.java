b200a064a599c7d96a09c4506229bda62f5c10ee
Denis <samparizona2018@yandex.ru>
2022-02-11 10:00:03 +0300

    added my third LabWork"

--- Main.java
+++ Main.java
@@ -2,52 +2,12 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите количество шагов");
-        int n = in.nextInt();
-        System.out.println("Введите значение для x");
-        int x = in.nextInt();
-        int minus = -1;
-        double down = 1;
-        double result = 0;
-
-        if (n < 0) {
-            System.out.print("n не может быть отрицательным числом.");
-        }
-
-        else
-        {
-            for (int a = 0; a <= n; a++)
-            {
-                double up = Math.pow((-1),a) * Math.pow(x,a);
-
-                down = getFactorial(a);
-                result = up / down;
-
-                String resultAll = String.format("%.2f", result);
-                System.out.println("x = " + x + ". " + "n = " + a);
-                System.out.println(resultAll);
+    public static void main(String[] args) {
+        for (int x = 1; x <= 9; x++) {
+            for (int y = 1; y <= 10; y++) {
+                System.out.println(x + " x " + y + " = " + (x * y));
             }
-        }
-
-    }
-
-
-    public static int getFactorial(int a)
-    {
-        if (a == 0) {
-            return 1;
-        } else
-        {
-            int result = 1;
-            for (int c = 1; c <= a; c++)
-            {
-                result = result * c;
-            }
-            return result;
+            System.out.println("===========");
         }
     }
 }
