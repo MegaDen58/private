e082f7cd5f0d6eaa687481efc90cf4e4630a98cf
Denis <samparizona2018@yandex.ru>
2022-02-25 11:32:31 +0300

    file added

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 package com.company;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        for (int x = 1; x <= 9; x++) {
-            for (int y = 1; y <= 10; y++) {
-                System.out.println(x + " x " + y + " = " + (x * y));
+        int[] array = new int[] {14, 43, 4, 9, 3, 31, 2, 13, 3, 4, 11, 54, 3, 55, 7};
+        minValue(array);
+    }
+    static void minValue(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < (array.length - 1); i++) {
+            if (array[i] < min) {
+                min = array[i];
             }
-            System.out.println("===========");
         }
+        System.out.println("Минимальное значение = " + min);
     }
 }
