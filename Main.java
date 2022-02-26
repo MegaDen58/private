3dffe3b294672b48c6b5fcdbee8f4723f40813fa
Denis <samparizona2018@yandex.ru>
2022-02-26 11:44:46 +0300

    added file

--- Main.java
+++ Main.java
@@ -7,16 +7,44 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[] {14, 43, 4, 9, 3, 31, 2, 13, 3, 4, 11, 54, 3, 55, 7};
-        minValue(array);
+        int[] array = createArray();
+        int length = array.length;
+        Arrays.sort(array);
+        length = removeDuplicateElements(array, length);
+        for (int i = 0; i < length; i++){
+            System.out.print(array[i] + " ");
+        }
+
     }
-    static void minValue(int[] array) {
-        int min = array[0];
-        for (int i = 0; i < (array.length - 1); i++) {
-            if (array[i] < min) {
-                min = array[i];
+    public static int removeDuplicateElements(int arr[], int n){
+        if (n==0 || n==1){
+            return n;
+        }
+        int[] tempA = new int[n];
+        int j = 0;
+        for (int i = 0; i < n - 1; i++){
+            if (arr[i] != arr[i + 1]){
+                tempA[j++] = arr[i];
             }
         }
-        System.out.println("Минимальное значение = " + min);
+        tempA[j++] = arr[n - 1];
+        for (int i = 0; i < j; i++){
+            arr[i] = tempA[i];
+        }
+        return j;
+    }
+    public static int[] createArray(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int length = in.nextInt();
+        int[] array = new int[length];
+        System.out.println("Заполните массив");
+        for (int i = 0; i < length; i++){
+            System.out.print("Введите " + i + " элемент: ");
+            array[i] = in.nextInt();
+        }
+        System.out.println("Вы создали массив: ");
+        System.out.println(Arrays.toString(array));
+        return array;
     }
 }
