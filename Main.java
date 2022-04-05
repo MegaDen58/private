95d64b10a8eb501098d118e4613e03762c89c8c2
Denis <samparizona2018@yandex.ru>
2022-04-05 17:24:14 +0300

    File added

--- Main.java
+++ Main.java
@@ -14,15 +14,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите число x (x > 2): ");
+        System.out.print("Введите число x: ");
         double x = in.nextInt();
-        if(x < 2){
-            System.out.println("Неверное значение x.");
+        System.out.print("Введите количество шагов (x > 2): ");
+        double n = in.nextInt();
+        if(n < 2){
+            System.out.println("Неверное значение n.");
         }
         else{
             double result = 0;
-            for(int i = 2; i <= x; i++){
-                result = result + (Math.pow(x, i) / factorial(i));
+            for(int i = 2; i <= ((int)n) + 1; i++){
+                if(i % 2 == 0) {
+                    result += (Math.pow(x, i) / factorial(i));
+                }
+                else{
+                    result -= (Math.pow(x, i) / factorial(i));
+                }
             }
             System.out.printf("%.3f", result);
         }
