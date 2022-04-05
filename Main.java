957aaf9324a89ed1a6c7810dd736349f9f7d84ba
Denis <samparizona2018@yandex.ru>
2022-04-05 17:52:33 +0300

    File added

--- Main.java
+++ Main.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 
 
 public class Main {
+
+    public static int factorial(int x){
+        int result = 1;
+        for(int i = 1; i <= x; i++){
+            result *= i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число x: ");
         double x = in.nextInt();
-        System.out.print("Введите количество шагов (x > 2): ");
+        System.out.print("Введите количество шагов (n >= 2): ");
         double n = in.nextInt();
         if(n < 2){
             System.out.println("Неверное значение n.");
@@ -15,12 +24,7 @@ public class Main {
         else{
             double result = 0;
             for(int i = 2; i <= ((int)n) + 1; i++){
-                if(i % 2 == 0) {
-                    result += (Math.pow(x, i) / i);
-                }
-                else{
-                    result -= (Math.pow(x, i) / i);
-                }
+                result += (Math.pow(x, 2 * i + 1) / factorial(2 * i + 1));
             }
             System.out.printf("%.3f", result);
         }
