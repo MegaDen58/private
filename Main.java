0ca0f0df7ebf0d4eeb77eb72ad1f0f262c43b099
Denis <samparizona2018@yandex.ru>
2022-04-07 22:26:26 +0300

    add

--- Main.java
+++ Main.java
@@ -6,8 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите предложение: ");
-        String word = in.nextLine();
-        String[] words = word.split(" ");
-        System.out.printf("В строчке %d слов(а).", words.length);
+        String str = in.nextLine();
+        System.out.print("Введите подстроку: ");
+        String underStr = in.nextLine();
+        int i = str.length() - underStr.length();
+        if(str.substring(i).equals(underStr)){
+            System.out.print("Введённая строка заканчиватся на введённую подстроку.");
+        }
+        else{
+            System.out.print("Введённая строка не заканчиватся на введённую подстроку.");
+        }
     }
 }
