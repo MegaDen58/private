54aa4938fed4c1f27b1a63b9d8291c2fa2da4b3a
Denis <samparizona2018@yandex.ru>
2022-04-07 22:09:27 +0300

    add

--- Main.java
+++ Main.java
@@ -7,37 +7,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите предложение: ");
         String word = in.nextLine();
-        int length = word.length();
-        char[] c = word.toCharArray();
-        int result = 0;
-        int k = length - 1;
-        if(length % 2 != 0){
-            int a = (length / 2) + 1;
-            for(int i = 0; i < a; i++, k--){
-                    if(c[i] == c[k]){
-                        result++;
-                }
-            }
-            if(result == (length / 2) + 1){
-                System.out.println("Число палиндром.");
-            }
-            else{
-                System.out.println("Число не палиндром.");
-            }
-        }
-        if(length % 2 == 0){
-            int a = length / 2;
-            for(int i = 0; k >= a; i++, k--){
-                if(c[i] == c[k]){
-                    result++;
-                }
-            }
-            if(result == length / 2){
-                System.out.println("Число палиндром.");
-            }
-            else{
-                System.out.println("Число не палиндром.");
-            }
-        }
+        String[] words = word.split(" ");
+        System.out.printf("В строчке %d слов(а).", words.length);
     }
 }
