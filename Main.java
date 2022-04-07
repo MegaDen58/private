0792743a40d1a6ae3a40e512a8fbdd8fb5f8cf57
Denis <samparizona2018@yandex.ru>
2022-04-07 19:42:26 +0300

    add

--- Main.java
+++ Main.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int n = in.nextInt();
-            if(n % 2 == 0){
-                System.out.println("Число чётное.");
+        String result = "";;
+        System.out.print("Введите предложение: ");
+        String word = in.nextLine();
+        int max = word.length();
+        String[] words = word.split(" ");
+        for(String t : words){
+            int c = t.length();
+            if(c < max){
+                max = c;
             }
-            if(n % 2 != 0){
-                System.out.println("Число нечётное.");
+        }
+        System.out.printf("Длина минимально слова равна %d символа(ов)\nЭти слова: \n", max);
+        for(String t : words){
+            if(t.length() == max){
+                result = t;
+                System.out.println(result);
             }
+        }
     }
 }
