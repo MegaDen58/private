768cfbf0b6a1b4671b294c7516ad45329227ca9e
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-05-06 07:32:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,59 +3,56 @@ package com.company;
 import java.io.*;
 import java.util.Scanner;
 
-
-class Function implements Serializable {
+class Function implements Serializable { // Создаём класса с переменными x, y и методом для нахождения y
     double x, y;
 
-    double getY() {
+    double getY() { // Находим y по формуле
         y = this.x - Math.sin(this.x);
         return y;
     }
 }
 
-
 public class Main {
-
     public static void main(String[] args) {
-        String text;
+        String txt;
         Scanner in = new Scanner(System.in);
-        Function func = new Function();
-        System.out.print("Введите значения: ");
-        int ss = 1;
+        Function func = new Function(); // Создаём объект класса
+        System.out.print("Enter value: ");
+        int i = 1;
         while (true) {
-            text = in.nextLine();
-            try {
-                double x = Double.parseDouble(text);
-                func.x = x;
-                func.getY();
-                if(ss == 1){
-                    System.out.println("Вы можете ввести одну из следующих комманд: save, upload, check, stop ");
-                    ss++;
+            txt = in.nextLine(); // Вводим текст
+            try { // Пробуем спарсить x в double и поместить это значение в x, который находится в классе. Просим ввести 1 из 4 команд
+                double x = Double.parseDouble(txt);
+                func.x = x; // Заносим значение введённого x в x, который находится в классе.
+                func.getY(); // Находим y 
+                if(i == 1){
+                    System.out.println("You can enter one of the following commands: save, upload, check, stop");
+                    i++;
                 }
-            } catch (Exception IOex) {
-                if (text.equalsIgnoreCase("save")) {
-                    try (ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream("file.txt"))) {
-                        write.writeObject(func);
-                        System.out.println("Данные сохранены в файл.");
-                    } catch (IOException ex) {
+            } catch (Exception IOe) { // Если x не спарсился в double или другая ошибка
+                if (txt.equalsIgnoreCase("save")) { // Если ввели слово save, записывает значения x и y в файл, как состояние объекта класса.
+                    try (ObjectOutputStream wr = new ObjectOutputStream(new FileOutputStream("object.txt"))) {
+                        wr.writeObject(func);
+                        System.out.println("Data saved to file");
+                    } catch (IOException ex) { // Если не получилось - выводим ошибку
                         ex.getMessage();
                     }
-                } else if (text.equalsIgnoreCase("upload")) {
-                    try (ObjectInputStream read = new ObjectInputStream(new FileInputStream("file.txt"))) {
-                        func = (Function) read.readObject();
-                        System.out.println("Всё нормально.");
-                    } catch (Exception ex) {
+                } else if (txt.equalsIgnoreCase("upload")) { // Если ввели upload, загружаем состояние объекта класса из файл в код
+                    try (ObjectInputStream rd = new ObjectInputStream(new FileInputStream("object.txt"))) {
+                        func = (Function) rd.readObject();
+                        System.out.println("Upload successful");
+                    } catch (Exception ex) { // Если не получилось - выводим ошибку
                         ex.getMessage();
                     }
-                } else if (text.equalsIgnoreCase("check")) {
+                } else if (txt.equalsIgnoreCase("check")) { // Если ввели check, выводим значения x и y
                     System.out.printf("x: %s\ny: %s\n",func.x, func.y);
-                } else if (text.equalsIgnoreCase("stop")){
+                } else if (txt.equalsIgnoreCase("stop")){ // Если ввели stop, программа останавливается
                     break;
                 }
-                else {
-                    System.out.println("Такой команды не существует.");
+                else { // Если ввели не ключевая слова - пишем, что такой команды нет
+                    System.out.println("Such a command does not exist");
                 }
-                System.out.println("Вы можете ввести одну из следующих комманд: save, upload, check, stop ");
+                System.out.println("You can enter one of the following commands: save, upload, check, stop"); // Просим ввести 1 из команд
             }
         }
     }
